df54ca5728bff3037489370565de1a301320453c
MoJafa <jmohsa@gmail.com>
2023-03-26 01:14:06 +0200

    added tacoorder to link tacos and customers

--- src/main/java/mo/jafa/tacocloud/TacoOrder.java
+++ src/main/java/mo/jafa/tacocloud/TacoOrder.java
@@ -1,2 +1,24 @@
-package mo.jafa.tacocloud;public class TacoOrder {
+package mo.jafa.tacocloud;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+    public void addTaco(Taco taco) {
+        tacos.add(taco);
+    }
+
 }
