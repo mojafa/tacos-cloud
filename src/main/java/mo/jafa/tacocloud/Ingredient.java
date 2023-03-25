1c3dd5cec7f023d4a25e4b85168efea3540f2413
MoJafa <jmohsa@gmail.com>
2023-03-26 01:13:21 +0200

    added ingredient class

---
+++ src/main/java/mo/jafa/tacocloud/Ingredient.java
@@ -0,0 +1,14 @@
+package mo.jafa.tacocloud;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
