7704b476a64def2813b47edb58846da32addb169
MoJafa <jmohsa@gmail.com>
2023-03-26 10:20:56 +0200

    injecting  repo into controller

--- src/main/java/mo/jafa/tacocloud/DesignTacoController.java
+++ src/main/java/mo/jafa/tacocloud/DesignTacoController.java
@@ -2,6 +2,7 @@ package mo.jafa.tacocloud;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,32 +17,28 @@ import java.util.stream.Collectors;
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepo;
 
-        @ModelAttribute
-        public void addIngredientsToModel(Model model) {
-            List<Ingredient> ingredients = Arrays.asList(
-                    new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                    new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                    new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                    new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                    new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                    new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                    new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                    new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                    new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                    new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-            );
-            Ingredient.Type[] types = Ingredient.Type.values();
-            for (Ingredient.Type type : types) {
-                model.addAttribute(type.toString().toLowerCase(),
-                        filterByType(ingredients, type));
-            }
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType((List<Ingredient>) ingredients, type));
         }
+    }
 
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order() {
         return new TacoOrder();
     }
+
     @ModelAttribute(name = "taco")
     public Taco taco() {
         return new Taco();
