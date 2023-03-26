53555228401b38600c76949b87435f94643d71f9
MoJafa <jmohsa@gmail.com>
2023-03-26 11:16:01 +0200

    added add ingreidients method, needd for tests topass

--- src/main/java/mo/jafa/tacocloud/Taco.java
+++ src/main/java/mo/jafa/tacocloud/Taco.java
@@ -3,25 +3,52 @@ package mo.jafa.tacocloud;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-@Table
 @Data
+@Table
+// Exclude createdAt from equals() method so that tests won't fail trying to
+// compare java.util.Date with java.sql.Timestamp (even though they're essentially
+// equal). Need to figure out a better way than this, but excluding this property
+// for now.
+@EqualsAndHashCode(exclude = "createdAt")
 public class Taco {
+
     @Id
     private Long id;
 
     private Date createdAt = new Date();
+
     @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
-    @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<IngredientRef> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient taco) {
+        this.ingredients.add(new IngredientRef(taco.getId()));
+    }
 
 }
+//@Table
+//@Data
+//public class Taco {
+//    @Id
+//    private Long id;
+//
+//    private Date createdAt = new Date();
+//    @NotNull
+//    @Size(min = 5, message = "Name must be at least 5 characters long")
+//    private String name;
+//
+//    @NotNull
+//    @Size(min = 1, message = "You must choose at least 1 ingredient")
+//    private List<Ingredient> ingredients;
+//
+//}
