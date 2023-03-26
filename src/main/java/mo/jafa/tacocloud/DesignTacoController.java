bdc954555f9c09ab15c2469d4979c9c0627eb05b
MoJafa <jmohsa@gmail.com>
2023-03-26 07:29:02 +0200

    added validation to post mapping of the controller

--- src/main/java/mo/jafa/tacocloud/DesignTacoController.java
+++ src/main/java/mo/jafa/tacocloud/DesignTacoController.java
@@ -1,12 +1,11 @@
 package mo.jafa.tacocloud;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -52,6 +51,26 @@ public class DesignTacoController {
     public String showDesignForm() {
         return "design";
     }
+
+//    @PostMapping
+//    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+//
+//        tacoOrder.addTaco(taco);
+//        log.info("Processing taco: " + taco);
+//        return "redirect:/orders/current";
+//    }
+    public String processTaco(
+            @Valid Taco taco, Errors errors,
+            @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}" + taco);
+
+        return  "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Ingredient.Type type) {
         return ingredients
@@ -59,6 +78,8 @@ public class DesignTacoController {
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
     }
+
+
 
 }
 
