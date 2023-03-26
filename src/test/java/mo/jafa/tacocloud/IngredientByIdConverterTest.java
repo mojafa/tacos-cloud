7eb94d2ce17ab762315cf02293b942999cedf14c
MoJafa <jmohsa@gmail.com>
2023-03-26 11:15:19 +0200

    added tests

---
+++ src/test/java/mo/jafa/tacocloud/IngredientByIdConverterTest.java
@@ -0,0 +1,38 @@
+package mo.jafa.tacocloud;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class IngredientByIdConverterTest {
+
+    private IngredientByIdConverter converter;
+
+    @BeforeEach
+    public void setup() {
+        IngredientRepository ingredientRepo = mock(IngredientRepository.class);
+        when(ingredientRepo.findById("AAAA"))
+                .thenReturn(Optional.of(new Ingredient("AAAA", "TEST INGREDIENT", Ingredient.Type.CHEESE)));
+        when(ingredientRepo.findById("ZZZZ"))
+                .thenReturn(Optional.empty());
+
+        this.converter = new IngredientByIdConverter(ingredientRepo);
+    }
+
+    @Test
+    public void shouldReturnValueWhenPresent() {
+        assertThat(converter.convert("AAAA"))
+                .isEqualTo(new Ingredient("AAAA", "TEST INGREDIENT", Ingredient.Type.CHEESE));
+    }
+
+    @Test
+    public void shouldReturnNullWhenMissing() {
+        assertThat(converter.convert("ZZZZ")).isNull();
+    }
+
+}
---
+++ src/test/java/mo/jafa/tacocloud/IngredientRepositoryTests.java
@@ -0,0 +1,35 @@
+package mo.jafa.tacocloud;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+@Transactional
+public class IngredientRepositoryTests {
+
+    @Autowired
+    IngredientRepository ingredientRepo;
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @Test
+    public void findById() {
+        Optional<Ingredient> flto = ingredientRepo.findById("FLTO");
+        assertThat(flto.isPresent()).isTrue();
+        assertThat(flto.get()).isEqualTo(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+
+        Optional<Ingredient> xxxx = ingredientRepo.findById("XXXX");
+        assertThat(xxxx.isEmpty()).isTrue();
+
+    }
+
+}
